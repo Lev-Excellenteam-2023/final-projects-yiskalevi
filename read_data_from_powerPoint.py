62000cd59c21b3c3c48f5173b70e168030333475
יסכה לוי <yilevi@g.jct.ac.il>
2023-05-12 12:31:30 +0300

    Building a general structure for models and functions

---
+++ read_data_from_powerPoint.py
@@ -0,0 +1,20 @@
+
+def openFile(path: str):
+    """
+    Gets a path to open a file * and opens the file checks that it is correct and exists.
+    """
+    pass
+
+
+def read_slides_from_presentation(file)->list:
+    """
+    A model reads the information from the presentation and returns a list in which each cell has a slide.
+    """
+    pass
+
+def read_data_from_slides(lst: list)->str:
+    """
+    The model will go over a slide and extract all the information from the text boxes.
+    """
+    pass
+
