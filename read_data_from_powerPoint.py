e17a5205c8098afac2e4283e6d4ca92973afad13
יסכה לוי <yilevi@g.jct.ac.il>
2023-05-22 02:15:08 +0300

    read from powerpoint

--- read_data_from_powerPoint.py
+++ read_data_from_powerPoint.py
@@ -1,20 +1,40 @@
+import os
+from pptx import Presentation
 
-def openFile(path: str):
-    """
-    Gets a path to open a file * and opens the file checks that it is correct and exists.
-    """
-    pass
+# def openFile(path: str):
+#     """
+#     Gets a path to open a file and opens the file, checks that it is correct and exists.
+#     """
+#     if os.path.isfile(path) and path.endswith('.pptx'):
+#         return True
+#     else:
+#         raise ValueError("Invalid file path or file format. Please provide a valid .pptx file.")
 
+def read_slides_from_presentation(file) -> list:
+    """
+    Reads the information from the presentation and returns a list where each element represents a slide.
+    """
+    presentation = Presentation(file)
+    slides = []
+    for slide in presentation.slides:
+        text_boxes = []
+        for shape in slide.shapes:
+            if shape.has_text_frame:
+                text_frame = shape.text_frame
+                text = ""
+                for paragraph in text_frame.paragraphs:
+                    for run in paragraph.runs:
+                        text += run.text
+                text_boxes.append(text.strip())
+        slides.append(text_boxes)
+    return slides
 
-def read_slides_from_presentation(file)->list:
-    """
-    A model reads the information from the presentation and returns a list in which each cell has a slide.
-    """
-    pass
-
-def read_data_from_slides(lst: list)->str:
-    """
-    The model will go over a slide and extract all the information from the text boxes.
-    """
-    pass
-
+# def read_data_from_slides(lst: list) -> str:
+#     """
+#     Extracts all the text information from the slides.
+#     """
+#     text = ""
+#     for slide in lst:
+#         for box in slide:
+#             text += box + "\n"
+#     return text.strip()
